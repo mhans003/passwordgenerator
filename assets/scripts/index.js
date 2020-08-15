cd68153402e7c2cebb50870e4d2b24a896a8ea6c
mhans003 <michaeledwardhanson@gmail.com>
2020-08-14 18:31:59 -0700

    Password generator works in console.

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -2,6 +2,7 @@
 //function uses random number generators to retrieve items 
 //function outputs password to screen 
 
+//PAGE ELEMENTS
 const submitButton = document.getElementById("submit-button"); 
 
 
@@ -35,14 +36,70 @@ function displayWarning(message) {
 function generatePassword() {
     //this function generates password, closes modal, and displays password on the screen
     
-    //access values 
-    //const length = Number(Math.floor(document.getElementById("inputLength").value));
-
+    //access values
+    //REFACTOR later 
+    const length = Number(Math.floor(document.getElementById("inputLength").value));
+    const isUppercase = document.getElementById("selectUppercase").checked; 
+    const isLowercase = document.getElementById("selectLowercase").checked; 
+    const isNumbers = document.getElementById("selectNumbers").checked; 
+    const isSpecialchar = document.getElementById("selectSpecial").checked; 
 
     //Create array to push all possible characters to, based on selections.
-    //let possibleCharacters = []; 
+    let possibleCharacters = []; 
+    let password = ""; 
 
-    console.log("Running generate password function"); 
+    console.log('Running password function.')
+
+    //Populate all possible characters 
+    if(isUppercase) {
+        //If the user selected uppercase letters, populate possible characters array with every uppercase letter.
+        for(let i = 65; i <= 90; i++) {
+            possibleCharacters.push(String.fromCharCode(i)); 
+        }
+    }
+
+    if(isLowercase) {
+        //If the user selected lowercase letters, populate possible characters array with every lowercase letter.
+        for(let i = 97; i <= 122; i++) {
+            possibleCharacters.push(String.fromCharCode(i)); 
+        }
+    }
+
+    if(isNumbers) {
+        //If the user selected numbers, populate possible characters array with every number.
+        for(let i = 48; i <= 57; i++) {
+            possibleCharacters.push(String.fromCharCode(i)); 
+        }
+    }
+
+    if(isSpecialchar) {
+        //If the user selected special characters, populate possible characters array with every special character.
+        for(let i = 33; i <= 47; i++) {
+            possibleCharacters.push(String.fromCharCode(i)); 
+        }
+        for(let i = 58; i <= 64; i++) {
+            possibleCharacters.push(String.fromCharCode(i)); 
+        }
+        for(let i = 91; i <= 96; i++) {
+            possibleCharacters.push(String.fromCharCode(i)); 
+        }
+        for(let i = 123; i <= 126; i++) {
+            possibleCharacters.push(String.fromCharCode(i)); 
+        }
+    }
+
+    console.log(possibleCharacters);
+    
+    //Generate password using available characters
+
+    for(let i = 0; i < length; i++) {
+        let randomIndex = Math.floor(Math.random() * possibleCharacters.length); 
+        //console.log(possibleCharacters[randomIndex]); 
+        password += possibleCharacters[randomIndex]; 
+    }
+
+    console.log(password); 
+    
 }
 
 
